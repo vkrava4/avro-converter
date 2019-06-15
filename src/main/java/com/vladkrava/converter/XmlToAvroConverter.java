25f199c87ebc9ee2f776ef03785de27c41471dd2
Vlad Krava <vkrava4@gmail.com>
2019-06-15 22:05:06 +0000

    Big Update: 1) added functionality for converting an XML to JSON and JSON to Avro 2) Unit tests refactoring 3) More test cases added to the suite

--- src/main/java/com/vladkrava/converter/XmlToAvroConverter.java
+++ src/main/java/com/vladkrava/converter/XmlToAvroConverter.java
@@ -1,16 +1,57 @@
 package com.vladkrava.converter;
 
+import static com.vladkrava.converter.AvroSchemaProcessor.processValue;
+
+import org.apache.avro.Schema;
 import org.apache.avro.specific.SpecificRecordBase;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.XML;
 
 import com.vladkrava.converter.serialization.AvroDeserializer;
+import com.vladkrava.converter.serialization.DataSerializationException;
 
 /**
+ * Converting data from XML string to specific Avro object of a type T
+ *
  * @author Vlad Krava - vkrava4@gmail.com
- * @since 0.1-SNAPSHOT
+ * @see SpecificRecordBase
+ * @see TargetConverter
+ * @see AvroDeserializer
+ * @see AvroSchemaProcessor
+ * @since 0.2-SNAPSHOT
  */
 public class XmlToAvroConverter<T extends SpecificRecordBase> extends AvroDeserializer<T> implements TargetConverter<T, String> {
+
+    /**
+     * Performs converting to specific Avro object
+     *
+     * @param s      - XML source {@link String}
+     * @param aClass - specific Avro object type
+     * @return T - specific Avro object
+     * @throws DataSerializationException - on exceptional conditions during deserialization
+     */
     @Override
-    public T convert(final String s, final Class<T> aClass) {
-        throw new UnsupportedOperationException("This operation in not supported yet.");
+    public T convert(final String s, final Class<T> aClass) throws DataSerializationException {
+        if (s == null) {
+            return null;
+        }
+
+        try {
+            final JSONObject jsonDataObject = XML.toJSONObject(s).getJSONObject(aClass.getSimpleName());
+            final JSONObject schemaJsonObject = new JSONObject();
+
+            if (jsonDataObject == null) {
+                throw new DataSerializationException("Bad object format");
+            }
+
+            for (final Schema.Field field : aClass.newInstance().getSchema().getFields()) {
+                schemaJsonObject.put(field.name(), processValue(jsonDataObject, field.schema(), field.name()));
+            }
+
+            return deserialize(schemaJsonObject.toString().getBytes(), aClass);
+        } catch (InstantiationException | JSONException | IllegalAccessException e) {
+            throw new DataSerializationException("An issue occurred with object deserialization", e);
+        }
     }
 }
--- src/main/java/com/vladkrava/converter/http/AbstractAvroHttpMessageConverter.java
+++ src/main/java/com/vladkrava/converter/http/AbstractAvroHttpMessageConverter.java
@@ -1,17 +1,13 @@
 package com.vladkrava.converter.http;
 
-import java.io.IOException;
+import static org.springframework.http.converter.StringHttpMessageConverter.DEFAULT_CHARSET;
 
 import org.apache.avro.specific.SpecificRecordBase;
-import org.apache.commons.compress.utils.IOUtils;
-import org.springframework.http.HttpInputMessage;
-import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.AbstractHttpMessageConverter;
 
 import com.vladkrava.converter.serialization.AvroDeserializer;
 import com.vladkrava.converter.serialization.AvroSerializer;
-import com.vladkrava.converter.serialization.DataSerializationException;
 
 /**
  * Implementation of {@link AbstractHttpMessageConverter} which can be used for reading and writing specific
@@ -23,16 +19,21 @@ import com.vladkrava.converter.serialization.DataSerializationException;
  * @see org.springframework.http.converter.HttpMessageConverter
  * @since 0.1-SNAPSHOT
  */
-public abstract class AbstractAvroHttpMessageConverter<T extends SpecificRecordBase> extends AbstractHttpMessageConverter<T> {
+public abstract class AbstractAvroHttpMessageConverter<T extends SpecificRecordBase>
+        extends AbstractHttpMessageConverter<T> {
 
-    private final AvroSerializer<SpecificRecordBase> avroSerializer;
-    private final AvroDeserializer<SpecificRecordBase> avroDeserializer;
+    private static final String APPLICATION = "application";
+    private static final String AVRO_JSON_SUBTYPE = "avro-json";
+    private static final String AVRO_XML_SUBTYPE = "avro-xml";
 
-    public AbstractAvroHttpMessageConverter(final MediaType supportedMediaType) {
+    static final MediaType AVRO_JSON_MEDIA_TYPE = new MediaType(APPLICATION, AVRO_JSON_SUBTYPE, DEFAULT_CHARSET);
+    static final MediaType AVRO_XML_MEDIA_TYPE = new MediaType(APPLICATION, AVRO_XML_SUBTYPE, DEFAULT_CHARSET);
+
+    public static final String AVRO_JSON = APPLICATION + "/" + AVRO_JSON_SUBTYPE;
+    public static final String AVRO_XML = APPLICATION + "/" + AVRO_XML_SUBTYPE;
+
+    AbstractAvroHttpMessageConverter(final MediaType supportedMediaType) {
         super(supportedMediaType);
-
-        this.avroSerializer = new AvroSerializer<>();
-        this.avroDeserializer = new AvroDeserializer<>();
     }
 
     /**
@@ -45,45 +46,5 @@ public abstract class AbstractAvroHttpMessageConverter<T extends SpecificRecordB
     @Override
     protected boolean supports(final Class<?> aClass) {
         return SpecificRecordBase.class.isAssignableFrom(aClass);
-    }
-
-    /**
-     * Reading accepted message and providing deserialization
-     *
-     * @param aClass       - target Avro object type
-     * @param inputMessage - accepted message
-     * @return {@code T} - converted Avro object
-     * @throws IOException - on read/write issues
-     */
-    @SuppressWarnings("unchecked")
-    @Override
-    protected T readInternal(final Class<? extends T> aClass, HttpInputMessage inputMessage) throws IOException {
-        T result = null;
-        final byte[] data = IOUtils.toByteArray(inputMessage.getBody());
-        if (data != null && data.length > 0) {
-            try {
-                result = (T) avroDeserializer.deserialize(data, aClass);
-            } catch (DataSerializationException e) {
-                logger.error(e.getMessage(), e);
-            }
-        }
-        return result;
-    }
-
-    /**
-     * Serializing and writing Avro object into outgoing message
-     *
-     * @param t                 - Avro object
-     * @param httpOutputMessage - outgoing message
-     * @throws IOException - on read/write issues
-     */
-    @Override
-    protected void writeInternal(final T t, final HttpOutputMessage httpOutputMessage) throws IOException {
-        try {
-            final byte[] data = avroSerializer.serialize(t);
-            httpOutputMessage.getBody().write(data);
-        } catch (DataSerializationException e) {
-            logger.error(e.getMessage(), e);
-        }
     }
 }
